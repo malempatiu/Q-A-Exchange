5fc122b891a80be8b8c71faa5ecfc39f65561ada
malempatiu <malempatiu@gmail.com>
2018-08-31 19:23:33 +0200

    created end points for user and added middleware logic

--- Q-A-Api/index.js
+++ Q-A-Api/index.js
@@ -1,11 +1,12 @@
 let env = process.env.NODE_ENV || 'development';
 if(env === 'development'){
     process.env.PORT = 8081;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/staffbase_api';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/q_a_api';
 } else if(env === 'test'){
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/staffbase_test_api';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/q_a_test_api';
 }
 //Importing external modules/librariers
+require('dotenv').config();
 const express = require('express'),
     app = express(),
     cors = require('cors'),
@@ -21,13 +22,16 @@ app.use(cors());
 //Importing local files
 const {createQuestion, fetchQuestions, getQuestion} = require('./handlers/user-questions');
 const {createAnswer} = require('./handlers/user-answers');
+const {createUser, loginUser} = require('./handlers/user-handler');
 
-//Questions Routes
+//User Routes
+app.post('/api/user/signup', createuser);
+app.post('/api/user/signin', loginUser);
+
+//Questions & Answers Routes
 app.post('/api/questions', createQuestion);
 app.get('/api/questions', fetchQuestions);
 app.get('/api/questions/:id', getQuestion);
-
-//Answers Routes
 app.post('/api/answers/:id', createAnswer);
 
 const port = process.env.PORT;
