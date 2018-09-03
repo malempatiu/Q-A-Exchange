2059482ca13657f002c143315f4f2acaf6ccb747
malempatiu <malempatiu@gmail.com>
2018-09-03 23:43:22 +0200

    passed test cases for the questions and answers actions

--- Q-A-Client/src/tests/actions/actions.test.js
+++ Q-A-Client/src/tests/actions/actions.test.js
@@ -1,23 +1,23 @@
-import { addError, removeError, setCurrentUser } from '../../actions/actions';
+import { addError, removeError, setCurrentUser, addQuestion, getQuestion, getQuestions, getUserQuestions, addAnswer } from '../../actions/actions';
 
 describe('ACTIONS TESTING', () => {
 
     describe('ERROR ACTIONS', () => {
-        it('should set up an object for the addError action', () => {
+        it('should setup an object for the addError action', () => {
             const addErrorActionObject = addError('unable to add error');
             expect(addErrorActionObject).toEqual({
                 type: 'ADD_ERROR',
                 error: 'unable to add error'
             });
         });
-        it('should set up an object for the removeError action', () => {
+        it('should setup an object for the removeError action', () => {
             const removeErrorActionObject = removeError();
             expect(removeErrorActionObject).toEqual({ type: 'REMOVE_ERROR' });
         });
     });
 
     describe('USER ACTIONS', () => {
-        it('should set up an object for the current user data', () => {
+        it('should setup an object for the current user data', () => {
             const user = {
                 id: '5432647589',
                 email: 'abc@gmail.com',
@@ -31,5 +31,98 @@ describe('ACTIONS TESTING', () => {
             });
         });
     });
-    
+
+    describe('QUESTIONS RELATED ACTIONS', () => {
+        it('should setup an object for the addQuestion action', () => {
+            const question = {
+                id: '2564789',
+                text: 'What is Testing?',
+                createdAt: 1536009421,
+                createdBy: 'abc',
+                answers: []
+            };
+            const addQuestionObject = addQuestion(question);
+            expect(addQuestionObject).toEqual({
+                type: 'ADD_QUESTION',
+                question
+            });
+        });
+        it('should setup an object for the getQuestion action', () => {
+            const question = {
+                id: '2564789',
+                text: 'What is Testing?',
+                createdAt: 1536009421,
+                createdBy: 'abc',
+                answers: [
+                    {
+                        id: '2564789',
+                        answer: 'ksdkfjaeipcnkvjn',
+                        createdAt: 1536009421,
+                        createdBy: 'abc',
+                    }
+                ]
+            };
+            const getQuestionObject = getQuestion(question);
+            expect(getQuestionObject).toEqual({
+                type: 'GET_QUESTION',
+                question
+            });
+        });
+        it('should setup an object for the getQuestions action', () => {
+            const questions = [
+                {
+                    id: '2564789',
+                    text: 'What is Testing?',
+                    createdAt: 1536009421,
+                    createdBy: 'abc',
+                    answers: [
+                        {
+                            id: '2564789',
+                            answer: 'ksdkfjaeipcnkvjn',
+                            createdAt: 1536009421,
+                            createdBy: 'abc',
+                        }
+                    ]
+                }
+            ];
+            const getQuestionsActionObject = getQuestions(questions);
+            expect(getQuestionsActionObject).toEqual({
+                type: 'GET_QUESTIONS',
+                questions
+            });
+        });
+        it('should setup an object for the getUserQuestions action', () => {
+            const userQuestions = [
+                {
+                    id: '2564789',
+                    text: 'What is Testing?',
+                    createdAt: 1536009421,
+                    createdBy: 'abc',
+                    answers: []
+                }
+            ];
+            const getUserQuestionsActionObject = getUserQuestions(userQuestions);
+            expect(getUserQuestionsActionObject).toEqual({
+                type: 'GET_USER_QUESTIONS',
+                userQuestions
+            });
+        });
+    });
+
+    describe('ANSWER ACTION', () => {
+        it('should setup an object for addAnswer action', () => {
+            const answer = {
+                id: '2564789',
+                answer: 'What is Testing?',
+                createdAt: 1536009421,
+                createdBy: 'abc'
+            };
+            const addAnswerActionObject = addAnswer(answer);
+            expect(addAnswerActionObject).toEqual({
+                type: 'ADD_ANSWER',
+                answer
+            });
+        });
+    });
+
 });
